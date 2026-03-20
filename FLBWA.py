a0d4a4cea8d88910a04e7083d0584a96ad58bd68
abdulmoktadir <abdulmoktadir2010@gmail.com>
2026-03-20 23:24:19 +0800

    Update FLBWA.py

--- FLBWA.py
+++ FLBWA.py
@@ -385,6 +385,7 @@ if run_model:
         st.subheader("2) Summary")
 
         c1, c2, c3 = st.columns(3)
+
         with c1:
             st.markdown('<div class="metric-card">', unsafe_allow_html=True)
             st.metric("Reference Factor", input_df.iloc[reference_index]["Factor"])
@@ -474,4 +475,4 @@ if run_model:
         st.markdown('</div>', unsafe_allow_html=True)
 
     except Exception as e:
-        st.error(f"An error occurred while running the model: {e}")"
+        st.error(f"An error occurred while running the model: {e}")
